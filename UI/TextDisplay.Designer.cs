15c3b564f0ba46568e6d02e1c25b9607666453bd
李倍存 <li.beicun@outlook.com>
2016-01-30 15:48:23 +0800

    初次提交

---
+++ UI/TextDisplay.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace Astrid.Common.UI {
+    public partial  class TextDisplay {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.rtxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rtxt
+            // 
+            this.rtxt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtxt.Location = new System.Drawing.Point(0, 0);
+            this.rtxt.Multiline = true;
+            this.rtxt.Name = "rtxt";
+            this.rtxt.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.rtxt.Size = new System.Drawing.Size(150, 150);
+            this.rtxt.TabIndex = 0;
+            // 
+            // TextDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.rtxt);
+            this.Name = "TextDisplay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox rtxt;
+    }
+}
---
+++ UI/TextDisplay.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Astrid.Common.UI {
+    public partial class TextDisplay : UserControl {
+        public TextDisplay() {
+            InitializeComponent();
+        }
+
+        public override Color ForeColor {
+            get {
+                return rtxt.ForeColor;
+            }
+            set {
+                rtxt.ForeColor = value;
+            }
+        }
+        public override Color BackColor {
+            get {
+                return rtxt.BackColor;
+            }
+            set {
+                rtxt.BackColor = value;
+            }
+        }
+
+        public void AppendLine(string str) {
+            AppendLine(str, Color.Green, Color.Black);
+        }
+        public void AppendLine(string str, Color foreColor, Color backColor) {
+            rtxt.AppendText(str);
+            rtxt.AppendText("\r\n");
+            ForeColor = foreColor;
+            BackColor = backColor;
+        }
+    }
+}
